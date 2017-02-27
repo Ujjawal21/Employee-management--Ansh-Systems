82fd4ccf8360e2b319f55d4efe240807764d0870
Ujjawal21 <ujjawalgayakwad@hotmail.com>
2017-02-27 11:53:33 +0530

    Initial Commit.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name employeeCrudApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the employeeCrudApp
+ */
+angular.module('employeeCrudApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
